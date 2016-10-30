6598010753333fb730eb1319508070227fe435bd
Alberto Costa <alberto.costa05@gmail.com>
2016-10-31 00:39:23 +0100

    creation of files: __init__.py, views.py, run.py

--- flask-startproject.py
+++ flask-startproject.py
@@ -5,16 +5,17 @@ if len(sys.argv) != 2:
     print "usage: {} <name>".format(sys.argv[0])
     sys.exit(1)
 else:
-    projectName = os.path.abspath(sys.argv[1])
-    if not os.path.exists(os.path.dirname(projectName)):
-        print 'folder: "{}" doesn\'t exist. aborting...'.format(os.path.dirname(projectName))
+    projectPath = os.path.abspath(sys.argv[1])
+    appName = os.path.basename(projectPath)
+    if not os.path.exists(os.path.dirname(projectPath)):
+        print 'folder: "{}" doesn\'t exist. aborting...'.format(os.path.dirname(projectPath))
         sys.exit(1)
-    if os.path.exists(projectName):
-        print 'File or folder with name "{}" already exists. aborting...'.format(projectName)
+    if os.path.exists(projectPath):
+        print 'File or folder with name "{}" already exists. aborting...'.format(projectPath)
         sys.exit(1)
     else:
-        os.mkdir(projectName)
-        appFolder = os.path.join(projectName, os.path.basename(projectName))
+        os.mkdir(projectPath)
+        appFolder = os.path.join(projectPath, appName)
         templatesFolder = os.path.join(appFolder, "templates")
         staticFolder = os.path.join(appFolder, "static")
         instanceFolder = os.path.join(appFolder, "instance")
@@ -23,3 +24,25 @@ else:
         os.mkdir(templatesFolder)
         os.mkdir(staticFolder)
         os.mkdir(instanceFolder)
+
+        initFilePath = os.path.join(appFolder, "__init__.py")
+        initFile = open(initFilePath, "w+")
+        initFile.write("from flask import Flask\n")
+        initFile.write("app = Flask(__name__)\n\n")
+        initFile.write("from {} import views".format(appName))
+        initFile.close()
+
+        viewsFilePath = os.path.join(appFolder, "views.py")
+        viewsFile = open(viewsFilePath, "w+")
+        viewsFile.write("from {} import app\n\n".format(appName))
+        viewsFile.write("@app.route('/')\n")
+        viewsFile.write("def index():\n")
+        viewsFile.write("    return 'Hello World'\n")
+        viewsFile.close()
+
+        runFilePath = os.path.join(projectPath, "run.py")
+        runFile = open(runFilePath, "w+")
+        runFile.write("#!/usr/bin/python\n")
+        runFile.write("from {} import app\n\n".format(appName))
+        runFile.write("app.run(debug=True, host='0.0.0.0')")
+        runFile.close()
